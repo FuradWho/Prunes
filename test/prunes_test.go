cdf138db4b5b6fa0baaadb75f09280b9a51dd8d0
Furad <1337543811@qq.com>
2022-04-19 15:56:39 +0800

    feat(uniswap.):uniswap v2

--- test/prunes_test.go
+++ test/prunes_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"fmt"
+	"github.com/FuradWho/prunes/factory"
 	"github.com/FuradWho/prunes/router"
 	"github.com/FuradWho/prunes/uniswap"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -17,16 +18,27 @@ import (
 	"log"
 	"math/big"
 	"testing"
+	"time"
 )
 
 var client *ethclient.Client
 
+var pairAddr common.Address
+var token0 common.Address
+var token1 common.Address
+
+const (
+	accountAddr   = "0x1d32A826a2Bf24C4d4CD7C8b5c72faC83C8ec3B3"
+	factoryV2Addr = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+	routerV2Addr  = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+	rinkebyAddr   = ""
+	mainnetAddr   = ""
+	key           = ""
+)
+
 func init() {
 	var err error
-	client, err = ethclient.Dial("https://eth-rinkeby.alchemyapi.io/v2/rI1IyR1NG8iHx-1RLlxPTh0FO1rCRzr3")
-
-	// https://eth-rinkeby.alchemyapi.io/v2/rI1IyR1NG8iHx-1RLlxPTh0FO1rCRzr3
-	// https://eth-mainnet.alchemyapi.io/v2/_VdQM-WC9xSSBwTDjV8SKjx2W5OjpOOi
+	client, err = ethclient.Dial(rinkebyAddr)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -34,57 +46,177 @@ func init() {
 	fmt.Println("we have a connection")
 }
 
+func Test_FactoryV2(t *testing.T) {
+
+	// 获取合约地址
+	contractAddr := common.HexToAddress(factoryV2Addr)
+
+	newFactory, err := factory.NewFactory(contractAddr, client)
+	if err != nil {
+		t.Error("new factory: ", err)
+		return
+	}
+
+	privateKey, err := crypto.HexToECDSA(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(4))
+	if err != nil {
+		t.Error("new auth: ", err)
+		return
+	}
+
+	session := factory.FactorySession{
+		Contract: newFactory,
+		CallOpts: bind.CallOpts{
+			Pending: true,
+		},
+		TransactOpts: bind.TransactOpts{
+			From:   auth.From,
+			Signer: auth.Signer,
+		},
+	}
+
+	// Returns the address of the nth pair (0-indexed) created through the factory, or address(0) (0x0000000000000000000000000000000000000000) if not enough pairs have been created yet.
+	pairAddr, err = session.AllPairs(big.NewInt(1))
+	if err != nil {
+		t.Error("get all pair: ", err)
+		return
+	}
+
+	fmt.Println("all pairs: ", pairAddr)
+
+	// Returns the total number of pairs created through the factory so far.
+	length, err := session.AllPairsLength()
+	if err != nil {
+		t.Error("get all pair len: ", err)
+		return
+	}
+	fmt.Println("get all pair len: ", length)
+
+}
+
+func Test_UniSwap(t *testing.T) {
+
+	contractAddr := common.HexToAddress("0x80f07c368BCC7F8CbAC37E79Ec332c1D84e9335D")
+
+	newUniswap, err := uniswap.NewUniswap(contractAddr, client)
+	if err != nil {
+		t.Error("new uniSwap: ", err)
+		return
+	}
+
+	privateKey, err := crypto.HexToECDSA(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(4))
+	if err != nil {
+		t.Error("new auth: ", err)
+		return
+	}
+
+	session := uniswap.UniswapSession{
+		Contract: newUniswap,
+		CallOpts: bind.CallOpts{
+			Pending: true,
+		},
+		TransactOpts: bind.TransactOpts{
+			From:   auth.From,
+			Signer: auth.Signer,
+		},
+	}
+
+	token0, err = session.Token0()
+	if err != nil {
+		t.Error("get Token0: ", err)
+		return
+	}
+	fmt.Println("token0 addr: ", token0)
+
+	token1, err = session.Token1()
+	if err != nil {
+		t.Error("get Token1: ", err)
+		return
+	}
+	fmt.Println("token1 addr: ", token1)
+
+}
+
+func Test_RouterV2(t *testing.T) {
+	contractAddr := common.HexToAddress(routerV2Addr)
+
+	newRouter, err := router.NewRouter(contractAddr, client)
+	if err != nil {
+		t.Error("new router: ", err)
+		return
+	}
+
+	privateKey, err := crypto.HexToECDSA(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(4))
+	if err != nil {
+		t.Error("new auth: ", err)
+		return
+	}
+
+	session := router.RouterSession{
+		Contract: newRouter,
+		CallOpts: bind.CallOpts{
+			Pending: true,
+		},
+		TransactOpts: bind.TransactOpts{
+			From:   auth.From,
+			Signer: auth.Signer,
+			Value:  big.NewInt(200),
+		},
+	}
+	// Returns the canonical WETH address on the Ethereum mainnet, or the Ropsten, Rinkeby, Görli, or Kovan testnets.
+	wethAddr, _ := session.WETH()
+	fmt.Println("weth addr: ", wethAddr)
+
+	/*
+		token0 addr:  0xc778417E063141139Fce010982780140Aa0cD5Ab
+		token1 addr:  0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85
+	*/
+
+	var addrs []common.Address
+	addrs = append(addrs,
+		common.HexToAddress("0xc778417E063141139Fce010982780140Aa0cD5Ab"),
+		common.HexToAddress("0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85"),
+	)
+
+	out, err := session.GetAmountsIn(big.NewInt(1), addrs)
+	if err != nil {
+		t.Error("get amount out: ", err)
+		return
+	}
+	fmt.Println(out)
+
+	transaction, err := session.SwapETHForExactTokens(big.NewInt(1), addrs, common.HexToAddress(accountAddr), big.NewInt(time.Now().Unix()+100))
+	if err != nil {
+		t.Error("swap eth :", err)
+		return
+	}
+	fmt.Println(transaction.Hash())
+}
+
 func Test_BalanceAt(t *testing.T) {
 
-	account := common.HexToAddress("0xC75650fe4D14017b1e12341A97721D5ec51D5340")
+	account := common.HexToAddress(accountAddr)
 
-	// 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
-	// 0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B
 	balance, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(balance) // 25893180161173005034
-
-	//address := common.HexToAddress("0x10ED43C718714eb63d5aA57B78B54704E256024E\n")
-	//instance, err := store.NewStore(address, client)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	newUniswap, err := uniswap.NewUniswap(account, client)
-	if err != nil {
-		fmt.Println("newUniswap：", err)
-		return
-	}
-
-	nonce, err := client.NonceAt(context.Background(), common.HexToAddress("0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B"), nil)
-	if err != nil {
-		fmt.Println("get nonce: ", err)
-	}
-	fmt.Println(nonce)
-	symbol, err := newUniswap.Symbol(&bind.CallOpts{
-		From:        common.HexToAddress("0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B"),
-		Pending:     true,
-		BlockNumber: big.NewInt(int64(nonce)),
-		Context:     context.Background(),
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(symbol)
-
-	//contractWithAbi, err := callContractWithAbi(client, privateKey, account)
-	//if err != nil {
-	//	fmt.Println("contractWithAbi :", err)
-	//	return
-	//}
-	//
-	//fmt.Println(contractWithAbi)
-
 }
 
 func callContractWithAbi(client *ethclient.Client, privKey *ecdsa.PrivateKey, from common.Address) (string, error) {
@@ -133,211 +265,5 @@ func callContractWithAbi(client *ethclient.Client, privKey *ecdsa.PrivateKey, fr
 	}
 
 	return signTx.Hash().Hex(), nil
-
-}
-
-func Test_Swap(t *testing.T) {
-
-	contractAddr := common.HexToAddress("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")
-
-	newUniswap, err := uniswap.NewUniswap(contractAddr, client)
-	if err != nil {
-		fmt.Println("new uniswap: ", err)
-		return
-	}
-
-	nonce, err := client.NonceAt(context.Background(), common.HexToAddress("0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B"), nil)
-	if err != nil {
-		fmt.Println("get nonce: ", err)
-	}
-
-	privateKey, err := crypto.HexToECDSA("")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(1))
-	if err != nil {
-		fmt.Println("new auth: ", err)
-		return
-	}
-
-	//newUniswapTransactor, err := uniswap.NewUniswapTransactor(contractAddr, client)
-	//if err != nil {
-	//	return
-	//}
-
-	session := uniswap.UniswapSession{
-		Contract: newUniswap,
-		CallOpts: bind.CallOpts{
-			Pending: true,
-		},
-		TransactOpts: bind.TransactOpts{
-			From:   auth.From,
-			Signer: auth.Signer,
-			Nonce:  big.NewInt(int64(nonce)),
-		},
-	}
-	//name, err := session.
-	//if err != nil {
-	//	fmt.Println("get name: ", err)
-	//	return
-	//}
-
-	swap, err := session.Swap(big.NewInt(1), big.NewInt(1), auth.From, nil)
-	if err != nil {
-		fmt.Println("swap err: ", err)
-		return
-	}
-	fmt.Println(swap.Nonce())
-
-	//fmt.Println(name)
-	//
-	//token0, _ := session.Token0()
-	//token1, _ := session.Token1()
-	//
-	//approve0, err := session.Approve(token0, big.NewInt(100))
-	//if err != nil {
-	//	fmt.Println("approve token0: ", err)
-	//	return
-	//}
-	//approve1, err := session.Approve(token1, big.NewInt(100))
-	//if err != nil {
-	//	fmt.Println("approve token1: ", err)
-	//	return
-	//}
-	//
-	//fmt.Println(approve1, approve0)
-
-	//tx, err := session.Swap(big.NewInt(1), big.NewInt(1), auth.From, nil)
-	//if err != nil {
-	//	fmt.Println("tx: ", err)
-	//	return
-	//}
-	//fmt.Println(tx.Hash())
-
-	//transaction, err := newUniswap.Swap(&bind.TransactOpts{
-	//	From:    auth.From,
-	//	Signer:  auth.Signer,
-	//	Nonce:   big.NewInt(int64(nonce)),
-	//	Context: context.Background(),
-	//}, big.NewInt(1), big.NewInt(1), auth.From, nil)
-	//if err != nil {
-	//	fmt.Println("transaction : ", err)
-	//	return
-	//}
-	//fmt.Println(transaction.Hash())
-
-	//
-	//newUniswap.Token0()
-
-	//initialize, err := newUniswapTransactor.Initialize(&bind.TransactOpts{
-	//	From:    auth.From,
-	//	Signer:  auth.Signer,
-	//	Nonce:   big.NewInt(int64(nonce)),
-	//	Context: context.Background(),
-	//}, common.HexToAddress("0x956F47F50A910163D8BF957Cf5846D573E7f87CA"), common.HexToAddress("0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B"))
-	//if err != nil {
-	//	fmt.Println("initialize: ", err)
-	//	return
-	//}
-	//fmt.Println(initialize.Nonce())
-	//fmt.Println(auth.From.Hex())
-	//transaction, err := newUniswapTransactor.Transfer(&bind.TransactOpts{
-	//	From:    auth.From,
-	//	Signer:  auth.Signer,
-	//	Nonce:   big.NewInt(int64(nonce)),
-	//	Context: context.Background(),
-	//}, auth.From, big.NewInt(1))
-	//if err != nil {
-	//	fmt.Println("transaction : ", err)
-	//	return
-	//}
-	//fmt.Println(transaction.Hash())
-
-}
-
-func Test_Router(t *testing.T) {
-	contractAddr := common.HexToAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-	newRouter, err := router.NewRouter(contractAddr, client)
-	if err != nil {
-		fmt.Println("new router: ", err)
-		return
-	}
-
-	privateKey, err := crypto.HexToECDSA("")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// auth := bind.NewKeyedTransactor(privateKey)
-
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(1))
-	if err != nil {
-		fmt.Println("new auth: ", err)
-		return
-	}
-
-	nonce, err := client.NonceAt(context.Background(), common.HexToAddress("0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B"), nil)
-	if err != nil {
-		fmt.Println("get nonce: ", err)
-		return
-	}
-
-	session := router.RouterSession{
-		Contract: newRouter,
-		CallOpts: bind.CallOpts{
-			Pending: true,
-		},
-		TransactOpts: bind.TransactOpts{
-			From:   auth.From,
-			Signer: auth.Signer,
-			Nonce:  big.NewInt(int64(nonce)),
-		},
-	}
-
-	fmt.Println(session.Receive())
-}
-func Test_RouterV2(t *testing.T) {
-
-	contractAddr := common.HexToAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-
-	newRouter, err := router.NewRouter(contractAddr, client)
-	if err != nil {
-		t.Error("new router : ", err)
-		return
-	}
-	privateKey, err := crypto.HexToECDSA("")
-	if err != nil {
-		t.Error(err)
-	}
-
-	// auth := bind.NewKeyedTransactor(privateKey)
-
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(4))
-	if err != nil {
-		t.Error("new auth: ", err)
-		return
-	}
-
-	nonce, err := client.NonceAt(context.Background(), common.HexToAddress("0x35d7D53cA0b9E122AE65E273cFE10F4a8f04454B"), nil)
-	if err != nil {
-		t.Error("get nonce: ", err)
-		return
-	}
-
-	session := router.RouterSession{
-		Contract: newRouter,
-		CallOpts: bind.CallOpts{
-			Pending: true,
-		},
-		TransactOpts: bind.TransactOpts{
-			From:   auth.From,
-			Signer: auth.Signer,
-			Nonce:  big.NewInt(int64(nonce)),
-		},
-	}
-
-	fmt.Println(session.Receive())
 
 }
